909ee9933b05ac83f47758e67e11a5e90ffc8798
Aryan030204 <artistaryan18@gmail.com>
2025-07-12 13:32:41 +0530

    updated

--- server/src/models/Feedback.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const feedbackSchema = new mongoose.Schema({
-  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  comment: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('Feedback', feedbackSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,3 @@
-// ✅ server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -8,22 +7,12 @@ const connectDB = require("./utils/db");
 // Load env variables
 dotenv.config();
 
-// Route imports
-const attendeeRoutes = require("./routes/attendeeRoutes");
-const organizerRoutes = require("./routes/organizerRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-
 // Initialize app
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Routes
-app.use("/api/attendee", attendeeRoutes);
-app.use("/api/organizer", organizerRoutes);
-app.use("/api/admin", adminRoutes);
 
 // Connect DB & start server
 const PORT = process.env.PORT || 5000;
