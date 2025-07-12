43d3ef971111cd1f468aa269f32c94b8f0effebb
Aryan030204 <artistaryan18@gmail.com>
2025-07-12 14:11:57 +0530

    add authentication routes and controller for user signup, login, and logout

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,98 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "error signing up",
+      error: err.message,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "you haven't registered yet",
+      });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      SameSite: "None",
+      secure: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Error logging in",
+      error: err.message,
+    });
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      expires: new Date(0),
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: err.message || err,
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./utils/db");
+const authRouter = require("./routes/authRoutes");
 
 // Load env variables
 dotenv.config();
@@ -13,6 +14,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.use("/api/", authRouter);
 
 // Connect DB & start server
 const PORT = process.env.PORT || 5000;
