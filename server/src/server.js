fac2f2de8a9fd9a49a86cf16b74169c355c09cc1
Aryan030204 <artistaryan18@gmail.com>
2025-07-15 09:40:33 +0530

    server.js updated

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://event-craft-event-management-tool-ia5a9qiw0.vercel.app"],
+    origin: ["https://event-craft-event-management-tool-qgd0niro3.vercel.app"],
     credentials: true,
   })
 );
